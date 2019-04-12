d76299ac5dff771895eb756c6c704c359eaef6c5
Jor Sanders <jorsanders@hotmail.com>
2019-04-13 00:30:05 +0200

    Added some sample php test pages

---
+++ docker-php/public_html/mysqli_test.php
@@ -0,0 +1,16 @@
+<p><a href="/">Index</a></p>
+<?php
+
+$servername = "mysql";
+$username = "dbuser";
+$password = "dbpass";
+$dbname = "dbname";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+var_dump($conn);
